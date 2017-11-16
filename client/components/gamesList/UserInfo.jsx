a8e3e7c9c26e4ef3aa4d2db5fd0f237de868e67a
Yusaku Kasahara <yusakukasahara@gmail.com>
2017-11-16 10:47:04 -0500

    added user and gameslist link on UserInfo

--- client/components/gamesList/UserInfo.jsx
+++ client/components/gamesList/UserInfo.jsx
@@ -1,15 +1,23 @@
 //This is a component which renders some user information at the top of the GamesView page
 import React from 'react';
+import { Route, browserHistory } from 'react-router';
+import { Link, IndexRoute, BrowserRouter as Router } from 'react-router-dom';
 
 // Gamesview -> GamesList -> Game
 //     \--> UserInfo
 
-const UserInfo = (props) => (
+const UserInfo = props => (
   <div className="row">
-     <h5 className="col s4">Username: {props.username}</h5>
-     <h5 className="col s4">Points: {props.points}</h5>
-     <h5 className="col s4">Level: {props.level}</h5>
+    <h5 className="col s3">
+      Username:
+      <Link to={`/profile/${props.username}`}> {props.username}</Link>
+    </h5>
+    <h5 className="col s3">
+      <Link to={`/gameList/`}>Code!</Link>
+    </h5>
+    <h5 className="col s3">Points: {props.points}</h5>
+    <h5 className="col s3">Level: {props.level}</h5>
   </div>
 );
 
-export default UserInfo; 
+export default UserInfo;
